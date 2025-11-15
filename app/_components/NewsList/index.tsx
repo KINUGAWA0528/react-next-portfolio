084c45446cdba11f33282716ad0488d7f6bca2c4
KINUGAWA0528 <ktc25a31e0005@edu.kyoto-tech.ac.jp>
2025-11-15 16:19:23 +0900

    feat: implement responsive layout and components for NewsList, Header, Footer, and Menu; add Category and Date components

--- app/_components/NewsList/index.tsx
+++ app/_components/NewsList/index.tsx
@@ -1,16 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 import styles from "./index.module.css";
+import Category from "../Category";
+import Date from "../Date";
+import { News } from "@/app/_libs/microcms";
 
-type News = {
-    id: string;
-    title: string;
-    category: {
-        name: string;
-    };
-    publishedAt: string;
-    createdAt: string;
-};
 
 type Props = {
     news: News[];
@@ -24,19 +19,16 @@ export default function NewsList({ news }: Props) {
         <ul>
             {news.map((article) => (
                 <li key={article.id} className={styles.list}>
-                    <div className={styles.link}>
+                    <Link href={`/news/${article.id}`} className={styles.link}>
                         <Image className={styles.image} src="/no-image.png" alt="No Image" width={1200} height={630} />
                         <dl className={styles.content}>
                             <dt className={styles.title}>{article.title}</dt>
                             <dd className={styles.meta}>
-                                <span className={styles.tag}>{article.category.name}</span>
-                                <span className={styles.date}>
-                                    <Image src="/clock.svg" alt="" width={16} height={16} priority />
-                                    {article.publishedAt}
-                                </span>
+                                <Category category={article.category} />
+                                <Date date={article.publishedAt} />
                             </dd>
                         </dl>
-                    </div>
+                    </Link>
                 </li>
             ))}
         </ul>
